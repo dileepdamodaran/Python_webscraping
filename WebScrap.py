25ff857304c81209ba63b953f0b4d60dee2ebc00
ddn <dileep.damodaran.n@gmail.com>
2020-12-24 12:43:12 -0500

    initial commit webscraping

---
+++ WebScrap.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import numpy as np
+
+URL = "https://stats.espncricinfo.com/ci/engine/records/averages/batting_bowling_by_team.html?id=13533;team=4345;type=tournament"
+page = requests.get(URL)
+bs = BeautifulSoup(page.content, 'lxml')
+
+t_head = bs.find_all('thead')
+for i, table in enumerate(t_head):
+    rows = table.find_all('th')
+    print(rows)
